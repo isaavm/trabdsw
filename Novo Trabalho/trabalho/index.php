ef7c00ba6a0087b82d03991b0890fe55f313acba
isaacvieiramorais@gmail.com <isaacvieiramorais@gmail.com@e15df9ca-12cf-73de-78c3-285f85265ed4>
2014-07-18 04:27:42 +0000

    Sessão e cookie na classe index.php FuncoesdeBanco.php com tratamento de erros

--- Novo Trabalho/trabalho/index.php
+++ Novo Trabalho/trabalho/index.php
@@ -1,27 +1,73 @@
 <?php
+ob_start();
+session_start();
+
+if (isset($_COOKIE["registro"])) {
+	if (empty($_SESSION['user'])) {
+		$codreg = base64_decode($_COOKIE['registro']);
+		include "FuncoesdeBanco.php";
+		$bd = new FuncoesdeBanco();
+		$se = $bd -> getById($codreg / 123);
+		if ($se['Codigo'] > -1) {
+			$se['id'] = session_id();
+			$se['ip'] = $_SERVER['REMOTE_ADDR'];
+			$se['hora'] = time();
+			$_SESSION['user'] = $se;
+		} else {
+			setcookie("registro", "", -3600);
+			echo "<meta HTTP-EQUIV='refresh' CONTENT='5;URL=controlaacesso.php'>";
+		}
+	}
+} elseif (isset($_POST['sendform'])) {
+	$nome = strtolower($_POST['Login']);
+	$pass = $_POST['Senha'];
+	include "FuncoesdeBanco.php";
+	$bd = new FuncoesdeBanco();
+	$se = $bd -> Logar($nome, $pass);
+	if ($se['Codigo'] > -1) {
+		$se['id'] = session_id();
+		$se['ip'] = $_SERVER['REMOTE_ADDR'];
+		$se['hora'] = time();
+		$_SESSION['user'] = $se;
+		setcookie("registro", base64_encode(((int)$_SESSION['user']['Codigo']) * 123));
+	} else {
+		echo "Login ou senha não conferem";
+		echo "<meta HTTP-EQUIV='refresh' CONTENT='5;URL=loginpage.php'>";
+	}
+}
+
+if (empty($_SESSION['user'])) {
+	header('Location: loginpage.php');
+} else {
+	$us = $_SESSION['user']['Nome'];
+	echo 'Ola ' . $us . '. sua sessao esta aberta!<br />';
+}
+
+//Isaac diz: Parei aqui (tirando o flush na ultima linha)
+
 include "default.php";
 $flag = 0;
 function Validacao($flag) {
-	if (isset($_COOKIE['classe']) && isset($_COOKIE['id'])) {
+	$ret = FALSE;
+	if (isset($_SESSION)) {
+		$flag = 1;
+	} elseif (isset($_COOKIE['codigo'])) {
 		echo "Os campos Classe e ID do COOKIE estão definidos!";
 		$flag = 1;
 	}
-	if (isset($_SESSION)) {
-		$flag = 1;
-	}
-	return $flag;
+	return $ret;
 }
 
 //////// PÁGINA DE LOGIN ////////////
 
-if (!Validacao($flag)) {
+if (!Validacao()) {
 	//include "loginpage.php";
-}else{
+} else {
 	include "controlaacesso.php";
 	$controla = new ControlaAcesso();
-	if($controla->ValiadaCookieSession()){
-		
-	}else{		
+	if ($controla -> ValiadaCookieSession()) {
+
+	} else {
 		include "loginpage.php";
 	}
 }
@@ -52,4 +98,5 @@ include "menuadmin.php";
 
 /////// RODAPÉ /////////////////////
 //include "bottom.php";
+ob_end_flush();
 ?>
