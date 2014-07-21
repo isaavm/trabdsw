bb040a803559749da266d358fd3680fb29ceff34
amzero_182@hotmail.com <amzero_182@hotmail.com@e15df9ca-12cf-73de-78c3-285f85265ed4>
2014-07-21 04:34:29 +0000

    cadastraraluno.php e Modelo E.R..mwb

--- Novo Trabalho/trabalho/cadastraraluno.php
+++ Novo Trabalho/trabalho/cadastraraluno.php
@@ -1,20 +1,32 @@
-<center>
-	<div id="planodefundocentral">
+<?php
+if(!empty($_POST['nome']) && !empty($_POST['nome']) && !empty($_POST['nome']) ){
+	include "FuncoesdeBanco.php";
+	$class = new FuncoesdeBanco();
+	if($class->CadastrarAluno($_POST['nome'], $_POST['matricula'], $_POST['$email'])){
+		echo "<script type='text/javascript'>.:Aluno cadastrado com sucesso!:.</script>";
+	}else{
+		echo "<script type='text/javascript'>.:Dados inválidos!:.</script>";
+	}
+}
+echo 
+"<center>
+	<div id='planodefundocentral'>
 		<h1>.:Cadastrar Aluno:.</h1>
-		<form action="#" method="post" form="formcadastaluno">
+		<form action='#' method='post' form='formcadastaluno'>
 			<p>
 				Nome:
-				<input type="text" name="nome" />
+				<input type='text' name='nome' />
 			</p>
 			<p>
 				Matrícula:
-				<input type="text" name="matricula" />
+				<input type='text' name='matricula' />
 			</p>
 			<p>
 				e-mail:
-				<input type="email" name="email" />
+				<input type='email' name='email' />
 			</p>
-			<input type="submit" value="Salvar"/>
+			<input type='submit' value='Salvar'/>
 		</form>
 	</div>
-</center>
+</center>";
+?>
