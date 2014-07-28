06d44c9364ac4b509cf81ec79f48563ba513b964
isaacvieiramorais@gmail.com <isaacvieiramorais@gmail.com@e15df9ca-12cf-73de-78c3-285f85265ed4>
2014-07-28 04:46:03 +0000

    atualização

--- trunk/Novo Trabalho/trabalho/logoff.php
+++ trunk/Novo Trabalho/trabalho/logoff.php
@@ -2,5 +2,6 @@
 session_start();
 session_destroy();
 print_r($_SESSION);
+setcookie("trabalhodsw", "", -3600);
 header('Location: loginpage.php');
 ?>
--- trunk/Novo Trabalho/trabalho/retornatrabalhos.php
+++ trunk/Novo Trabalho/trabalho/retornatrabalhos.php
@@ -1,15 +1,10 @@
 <?php
 session_start();
-echo "foi-se";
 include_once "FuncoesdeBanco.php";
 $class = new FuncoesdeBanco();
-echo "nois";
-$turma = $_POST['turma'];
-$disciplina = $_POST['disciplina'];
-$codTurma = $class->GetCodTurmaByDisciplina($disciplina, $turma);
-//echo "<option id='$codTurma'>$codTurma - $disciplina - </option>";
-//$trabalhos = $class->GetTrabalhosByTurma($codTurma);
+$turma = $_POST['idturma'];
+$codTurma = $class->getCodNomeTrabalhosByTurma($turma);
 foreach ($codTurma as $value) {
-	echo "<option id='$value'>$value</option>";
+	echo "<option id='".$value['id']."'>".$value['titulo']."</option>";
 }
 ?>
